c30848f754e2d71c0bdb376d0189857d09fafb75
Josh <josh.n.williams@gmail.com>
2017-05-30 23:04:45 -0700

    Update index.php

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ define('RASPI_ADMIN_DETAILS', RASPI_CONFIG.'/raspap.auth');
 
 // Constants for configuration file paths.
 // These are typical for default RPi installs. Modify if needed.
-define('RASPI_DNSMASQ_CONFIG', '/etc/dnsmasq.conf');
+define('RASPI_DNSMASQ_CONFIG', '/etc/dnsmasq.d/access_point.conf');
 define('RASPI_DNSMASQ_LEASES', '/var/lib/misc/dnsmasq.leases');
-define('RASPI_HOSTAPD_CONFIG', '/etc/hostapd/hostapd.conf');
+define('RASPI_HOSTAPD_CONFIG', '/etc/hostapd.conf');
 define('RASPI_WPA_SUPPLICANT_CONFIG', '/etc/wpa_supplicant/wpa_supplicant.conf');
 define('RASPI_HOSTAPD_CTRL_INTERFACE', '/var/run/hostapd');
 define('RASPI_WPA_CTRL_INTERFACE', '/var/run/wpa_supplicant');
