9d4f6a44fb0df81ab5e7e0a836bd73eb3466f1d8
Josh <josh.n.williams@gmail.com>
2017-05-31 10:05:00 -0700

    Update openvpn.php

--- includes/openvpn.php
+++ includes/openvpn.php
@@ -1,1 +1,131 @@
 
+/**
+*
+*
+*/
+function DisplayOpenVPNConfig() {
+
+	exec( 'cat '. RASPI_OPENVPN_CLIENT_CONFIG, $returnClient );
+	exec( 'cat '. RASPI_OPENVPN_SERVER_CONFIG, $returnServer );
+	exec( 'pidof openvpn | wc -l', $openvpnstatus);
+
+	if( $openvpnstatus[0] == 0 ) {
+		$status = '<div class="alert alert-warning alert-dismissable">OpenVPN is not running
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>';
+	} else {
+		$status = '<div class="alert alert-success alert-dismissable">OpenVPN is running
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>';
+	}
+
+	// parse client settings
+	foreach( $returnClient as $a ) {
+		if( $a[0] != "#" ) {
+			$arrLine = explode( " ",$a) ;
+			$arrClientConfig[$arrLine[0]]=$arrLine[1];
+		}
+	}
+
+	// parse server settings
+	foreach( $returnServer as $a ) {
+		if( $a[0] != "#" ) {
+			$arrLine = explode( " ",$a) ;
+			$arrServerConfig[$arrLine[0]]=$arrLine[1];
+		}
+	}
+	?>
+	<div class="row">
+	<div class="col-lg-12">
+    	<div class="panel panel-primary">           
+			<div class="panel-heading"><i class="fa fa-lock fa-fw"></i> Configure OpenVPN 
+            </div>
+        <!-- /.panel-heading -->
+        <div class="panel-body">
+        	<!-- Nav tabs -->
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#openvpnclient" data-toggle="tab">Client settings</a>
+                </li>
+                <li><a href="#openvpnserver" data-toggle="tab">Server settings</a>
+                </li>
+            </ul>
+            <!-- Tab panes -->
+           	<div class="tab-content">
+           		<p><?php echo $status; ?></p>
+            	<div class="tab-pane fade in active" id="openvpnclient">
+            		
+            		<h4>Client settings</h4>
+					<form role="form" action="?page=save_hostapd_conf" method="POST">
+
+					<div class="row">
+						<div class="form-group col-md-4">
+	                        <label>Select OpenVPN configuration file (.ovpn)</label>
+	                        <input type="file" name="openvpn-config">
+	                    </div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">Client Log</label>
+							<input type="text" class="form-control" id="disabledInput" name="log-append" type="text" placeholder="<?php echo $arrClientConfig['log-append']; ?>" disabled />
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fade" id="openvpnserver">
+            		<h4>Server settings</h4>
+            		<div class="row">
+						<div class="form-group col-md-4">
+            			<label for="code">Port</label> 
+            			<input type="text" class="form-control" name="openvpn_port" value="<?php echo $arrServerConfig['port'] ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+						<label for="code">Protocol</label>
+						<input type="text" class="form-control" name="openvpn_proto" value="<?php echo $arrServerConfig['proto'] ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+						<label for="code">Root CA certificate</label>
+						<input type="text" class="form-control" name="openvpn_rootca" placeholder="<?php echo $arrServerConfig['ca']; ?>" disabled />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+						<label for="code">Server certificate</label>
+						<input type="text" class="form-control" name="openvpn_cert" placeholder="<?php echo $arrServerConfig['cert']; ?>" disabled />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+						<label for="code">Diffie Hellman parameters</label>
+						<input type="text" class="form-control" name="openvpn_dh" placeholder="<?php echo $arrServerConfig['dh']; ?>" disabled />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+						<label for="code">KeepAlive</label>
+						<input type="text" class="form-control" name="openvpn_keepalive" value="<?php echo $arrServerConfig['keepalive']; ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+						<label for="code">Server log</label>
+						<input type="text" class="form-control" name="openvpn_status" placeholder="<?php echo $arrServerConfig['status']; ?>" disabled />
+						</div>
+					</div>
+            	</div>
+				<input type="submit" class="btn btn-outline btn-primary" name="SaveOpenVPNSettings" value="Save settings" />
+				<?php
+				if($hostapdstatus[0] == 0) {
+					echo '<input type="submit" class="btn btn-success" name="StartOpenVPN" value="Start OpenVPN" />';
+				} else {
+					echo '<input type="submit" class="btn btn-warning" name="StopOpenVPN" value="Stop OpenVPN" />';
+				}
+				?>
+				</form>
+		</div><!-- /.panel-body -->
+    </div><!-- /.panel-primary -->
+    <div class="panel-footer"> Information provided by openvpn</div>
+</div><!-- /.col-lg-12 -->
+</div><!-- /.row -->
+<?php
+}
